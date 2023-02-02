79b79280fd1b73249c56e970c5fc298de9d8c541
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-01 21:42:54 -0500

    uncommenting code

--- client/src/App.js
+++ client/src/App.js
@@ -1,34 +1,34 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 // Uncomment import statement below after building queries and mutations
-// import { ApolloClient, ApolloProvider, InMemoryCache } from '@apollo/client';
+import { ApolloClient, ApolloProvider, InMemoryCache } from "@apollo/client";
 
-import Home from './pages/Home';
-import Matchup from './pages/Matchup';
-import Vote from './pages/Vote';
+import Home from "./pages/Home";
+import Matchup from "./pages/Matchup";
+import Vote from "./pages/Vote";
 
 function App() {
-  return (
-    <Router>
-      <div className="flex-column justify-center align-center min-100-vh bg-primary">
-        <Routes>
-          <Route 
-            path="/" 
-            element={<Home />} 
-          />
-          <Route 
-            path="/matchup" 
-            element={<Matchup />} 
-          />
-          <Route 
-            path="/matchup/:id" 
-            element={<Vote />} 
-          />
-        </Routes>
-      </div>
-    </Router>
-  );
+	return (
+		<Router>
+			<div className="flex-column justify-center align-center min-100-vh bg-primary">
+				<Routes>
+					<Route
+						path="/"
+						element={<Home />}
+					/>
+					<Route
+						path="/matchup"
+						element={<Matchup />}
+					/>
+					<Route
+						path="/matchup/:id"
+						element={<Vote />}
+					/>
+				</Routes>
+			</div>
+		</Router>
+	);
 }
 
 export default App;
