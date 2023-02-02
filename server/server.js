a3166b1b284e28d7716f3c5f557882d52bd6764c
pbp66 <{ID}+{username}@users.noreply.github.com>
2023-02-01 21:41:41 -0500

    created typeDefs.js resolvers.js

--- server/server.js
+++ server/server.js
@@ -1,14 +1,11 @@
-const express = require('express');
-// Uncomment the following code once you have built the queries and mutations in the client folder
-// const { ApolloServer } = require('apollo-server-express');
-const path = require('path');
-
-// Uncomment the following code once you have built the queries and mutations in the client folder
-// const { typeDefs, resolvers } = require('./schemas');
-const db = require('./config/connection');
+const express = require("express");
+const { ApolloServer } = require("apollo-server-express");
+const path = require("path");
+const { typeDefs, resolvers } = require("./schemas");
+const db = require("./config/connection");
 
 // Comment out this code once you have built out queries and mutations in the client folder
-const routes = require('./routes');
+// const routes = require('./routes');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -24,8 +21,8 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // if we're in production, serve client/build as static assets
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, '../client/build')));
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "../client/build")));
 }
 
 // Comment out this code once you have built out queries and mutations in the client folder
@@ -51,8 +48,8 @@ const startApolloServer = async (typeDefs, resolvers) => {
 */
 
 // Comment out this code once you have built out queries and mutations in the client folder
-db.once('open', () => {
-  app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
+db.once("open", () => {
+	app.listen(PORT, () => console.log(`Now listening on localhost: ${PORT}`));
 });
 
 // Uncomment the following code once you have built the queries and mutations in the client folder
